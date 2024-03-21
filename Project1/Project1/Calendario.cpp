aeacd8e6d58e36fd5b8c0f3180feef9834c9b343
Nicolas Quintanilla <107016019+Niclaii@users.noreply.github.com>
2024-03-20 23:14:53 -0500

    Add files via upload

--- Project1/Project1/Calendario.cpp
+++ Project1/Project1/Calendario.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
+#include "Calendario.h"
 
-main()
+using namespace std;
+
+int main()
 {
+	int dia{ 0 }, mes{ 0 }, year{ 0 };
 
+	cout << "ingresa la fecha que desees (dia) ";
+	cin >> dia;
 
+	cout << "ingresa la fecha que desees (mes) ";
+	cin >> mes;
 
+	cout << "ingresa la fecha que desees (año) ";
+	cin >> year;
 
-
-
-
-
-
-
-
-
+	ImprimirCalendario(dia, mes, year);
 
 	return 0;
 }
--- Project1/Project1/Calendario.h
+++ Project1/Project1/Calendario.h
@@ -1,1 +1,83 @@
 #pragma once
+#include <iostream>
+
+using namespace std;
+
+void mesLetras(int mes)
+{
+	string ML{""};
+	if (mes == 1)
+	{
+		ML = "Enero";
+	}
+	else if (mes == 2)
+	{
+		ML = "Febrero";
+	}
+	else if (mes == 3)
+	{
+		ML = "Marzo";
+	}
+	else if (mes == 4)
+	{
+		ML = "Abril";
+	}
+	else if (mes == 5)
+	{
+		ML = "Mayo";
+	}
+	else if (mes == 6)
+	{
+		ML = "Junio";
+	}
+	else if (mes == 7)
+	{
+		ML = "Julio";
+	}
+	else if (mes == 8)
+	{
+		ML = "Agosto";
+	}
+	else if (mes == 9)
+	{
+		ML = "Septiembre";
+	}
+	else if (mes == 10)
+	{
+		ML = "Octubre";
+	}
+	else if (mes == 11)
+	{
+		ML = "Noviembre";
+	}
+	else if (mes == 12)
+	{
+		ML = "Diciembre";
+	}
+	return;
+}
+
+void ImprimirCalendario(int dia,int mes,int año)
+{
+	int a{ 1 }, x[31];
+
+	cout << "\nDom " << "Lun " << "Mar " << "Mie " << "Jue " << "Vie " << "Sab \n";
+
+
+
+	for (int i{ 1 }; i < 31; i++)
+	{
+
+
+
+	}
+	return;
+}
+
+int CalcularDias(int mes, int a)
+{
+	if (mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes == 12)
+	{
+
+	}
+}
